ad33cd030ce1e45bd28bb9f1c3ebceea56948feb
Kasia <katarzyna.kulma@gmail.com>
2018-01-08 22:13:34 +0000

    finished draft

--- clinton_trump_tweets_prediction&interpretation.R
+++ clinton_trump_tweets_prediction&interpretation.R
@@ -8,7 +8,8 @@ library(lime)
 library(xgboost) # the classifier
 library(caret)
 library(dplyr)
-#library(text2vec)
+library(text2vec)
+
 
 
 tweets <- read_csv('tweets.csv')
@@ -18,8 +19,11 @@ table(tweets$handle)
 
 all_tweets <- tweets %>% 
   rename(author = handle) %>% 
-  select(author, text)
+  select(author, text) %>% 
+  na.omit()
 
+summary(all_tweets)
+head(all_tweets)
 
 set.seed(1234)
 trainIndex <- createDataPartition(all_tweets$author, p = .8, 
@@ -35,10 +39,17 @@ str(test_tweets)
 
 # Tokenize data
 
+get_matrix2 <- function(text) {
+  it <- itoken(text, tolower, progressbar = TRUE)
+  create_dtm(it, vectorizer = hash_vectorizer(ngram = c(1L, 2L)))
+}
 
 # BoW matrix generation
-dtm_train = get_matrix(train_sentences$text)
-dtm_test = get_matrix(test_sentences$text)
+dtm_train2 = get_matrix2(train_tweets$text)
+dtm_test2 = get_matrix2(test_tweets$text)
+
+dim(dtm_train2)
+dim(dtm_test2)
 
 # Create boosting model for binary classification (-> logistic loss)
 # Other parameters are quite standard
@@ -47,28 +58,31 @@ param <- list(max_depth = 7,
               objective = "binary:logistic", 
               eval_metric = "error", 
               nthread = 1)
-
-xgb_model <- xgb.train(
+set.seed(1234)
+xgb_model2 <- xgb.train(
   param, 
-  xgb.DMatrix(dtm_train, label = train_sentences$class.text == "OWNX"),
+  xgb.DMatrix(dtm_train2, label = train_tweets$author == "realDonaldTrump"),
   nrounds = 50
 )
 
 
 # We use a (standard) threshold of 0.5
-predictions <- predict(xgb_model, dtm_test) > 0.5
-test_labels <- test_sentences$class.text == "OWNX"
+predictions <- predict(xgb_model2, dtm_test2) > 0.5
+test_labels <- test_tweets$author == "realDonaldTrump"
 
 # Accuracy
 print(mean(predictions == test_labels))
 
 
 # We select 10 sentences from the label OWNX
-sentence_to_explain <- head(test_sentences[test_labels,]$text, 5)
-explainer <- lime(sentence_to_explain, model = xgb_model, 
-                  preprocess = get_matrix)
+
+detach("package:dplyr", unload=TRUE)
+
+sentence_to_explain <- head(test_tweets[test_labels,]$text, 5)
+explainer <- lime(sentence_to_explain, model = xgb_model2, 
+                  preprocess = get_matrix2)
 explanation <- explain(sentence_to_explain, explainer, n_labels = 1, 
-                       n_features = 2)
+                       n_features = 5)
 
 # Most of the words choosen by Lime
 # are related to the team (we, our)
